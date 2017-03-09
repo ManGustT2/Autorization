0647007d73a09b8c18177eb8449e2b3fd40c363f
test <vadklim@i.ua>
2017-03-09 23:54:15 +0200

    ready fb

--- app/src/main/java/com/mangust/autorization/MainActivity.java
+++ app/src/main/java/com/mangust/autorization/MainActivity.java
@@ -8,14 +8,15 @@ import android.widget.TextView;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
-    LoginButton loginButton;
-    CallbackManager callbackManager;
-    TextView text;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextView text;
 
 
 
@@ -23,33 +24,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
         callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        text = (TextView) findViewById(R.id.textView);
-
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                text.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken());
 
             }
 
             @Override
             public void onCancel() {
-
+                text.setText("Login attempt canceled.");
             }
 
             @Override
             public void onError(FacebookException error) {
+                text.setText("Login attempt failed.");
 
             }
 
              });
-
-
-
 
     }
 
@@ -57,5 +62,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
+   }
 }
