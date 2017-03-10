9a9d4cf0eb86c62914ca1df95a3970c08f13d577
test <vadklim@i.ua>
2017-03-11 00:43:21 +0200

    google with

--- app/src/main/java/com/mangust/autorization/MainActivity.java
+++ app/src/main/java/com/mangust/autorization/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         loginButton.setReadPermissions("email");
 
         mStatusTextView = (TextView) findViewById(R.id.textView);
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         callbackManager = CallbackManager.Factory.create();
 
@@ -101,15 +100,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
         if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
             Log.d(TAG, "Got cached sign-in");
             GoogleSignInResult result = opr.get();
             handleSignInResult(result);
         } else {
-            // If the user has not previously signed in on this device or the sign-in has expired,
-            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-            // single sign-on will occur in this branch.
+
             showProgressDialog();
             opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
                 @Override
